b081e2801897b91c14cf78ed842bd8c1db6c87b7
Danielle Barda <dbarda@redhat.com>
2024-08-01 18:28:52 +0300

    Add gather information role

---
+++ iso-installation-scripts/installation_collections_as_user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ansible-galaxy collection install community.vmware
+ansible-galaxy collection install vmware.vmware_rest
+ansible-galaxy collection install vmware.vmware
+ansible-galaxy collection install community.general
+
---
+++ iso-installation-scripts/installation_script_as_root.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo dnf install -y python3-pip vim
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+sudo dnf install -y ansible
+
+pip install aiohttp 
+pip3 install pyvmomi
+
+mkdir ~/ansible-playbooks
+
+
