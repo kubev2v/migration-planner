3ecd2aac59e9208d908b87cc4bc5e989d72548cb
Danielle Barda <dbarda@redhat.com>
2024-08-06 14:28:38 +0300

    Aggregation report
    Signed-off-by: Danielle Barda <dbarda@redhat.com>

--- iso-installation-scripts/installation_script_as_root.sh
+++ iso-installation-scripts/installation_script_as_root.sh
@@ -2,11 +2,9 @@
 
 sudo dnf install -y python3-pip vim
 sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-sudo dnf install -y ansible
+sudo dnf install -y ansible podman
 
 pip install aiohttp 
 pip3 install pyvmomi
 
 mkdir ~/ansible-playbooks
-
-
