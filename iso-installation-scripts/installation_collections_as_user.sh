e9ffa3d8bb7d94232a779927d5f944141d8e92a5
Ondra Machacek <omachace@redhat.com>
2024-08-21 21:45:11 +0200

    Remove ansible stuff
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- iso-installation-scripts/installation_collections_as_user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ansible-galaxy collection install community.vmware
-ansible-galaxy collection install vmware.vmware_rest
-ansible-galaxy collection install vmware.vmware
-ansible-galaxy collection install community.general
-
