a1d339eaf7bd4d4050d395cd37dd3e113ce7f4ba
Jonathan Kilzi <jkilzi@redhat.com>
2024-09-12 08:58:57 +0300

    chore: Updates the planner-db deployment script
    Details:
    - Remove the top-level version field because it is obsolete (see https://github.com/compose-spec/compose-spec/blob/main/spec.md#version-top-level-element-obsolete) and clutters the logs
    - Moves the volume definition from the makefile into compose.yaml
    - Adds host.docker.internal to /etc/hosts to facilitate debugging the DB
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- deploy/deploy.mk
+++ deploy/deploy.mk
@@ -1,13 +1,12 @@
 deploy-db:
 	podman rm -f planner-db || true
 	podman volume rm podman_planner-db || true
-	podman volume create --opt device=tmpfs --opt type=tmpfs --opt o=nodev,noexec podman_planner-db
-	cd deploy/podman && podman-compose up -d planner-db
+	podman-compose -f deploy/podman/compose.yaml up -d planner-db
 	test/scripts/wait_for_postgres.sh podman
 	podman exec -it planner-db psql -c 'ALTER ROLE admin WITH SUPERUSER'
 	podman exec -it planner-db createdb admin || true
 
 kill-db:
-	cd deploy/podman && podman-compose down planner-db
+	podman-compose -f deploy/podman/compose.yaml down planner-db
 
 .PHONY: deploy-db kill-db
