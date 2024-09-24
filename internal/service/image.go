0ef485947290faebd8d9b9808289ac7d67de7dd1
Ondra Machacek <omachace@redhat.com>
2024-09-24 20:04:16 +0200

    Return HTTP 500 for error in ova generate
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- internal/service/image.go
+++ internal/service/image.go
@@ -13,11 +13,11 @@ import (
 func (h *ServiceHandler) GetSourceImage(ctx context.Context, request server.GetSourceImageRequestObject) (server.GetSourceImageResponseObject, error) {
 	writer, ok := ctx.Value(image.ResponseWriterKey).(http.ResponseWriter)
 	if !ok {
-		return server.GetSourceImage400JSONResponse{Message: "error creating the HTTP stream"}, nil
+		return server.GetSourceImage500JSONResponse{Message: "error creating the HTTP stream"}, nil
 	}
 	ova := &image.Ova{Id: request.Id, Writer: writer}
 	if err := ova.Generate(); err != nil {
-		return server.GetSourceImage400JSONResponse{Message: fmt.Sprintf("error generating image %s", err)}, nil
+		return server.GetSourceImage500JSONResponse{Message: fmt.Sprintf("error generating image %s", err)}, nil
 	}
 	return server.GetSourceImage200ApplicationoctetStreamResponse{Body: bytes.NewReader([]byte{})}, nil
 }
