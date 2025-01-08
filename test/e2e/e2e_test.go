21148e1bd28ef3cabe39a141dda1783e3ff80250
Aviel Segev <asegev@redhat.com>
2025-01-08 15:12:07 +0200

    Improve the end-to-end tests and fix the issue with 'BeforeEach', which causes errors when running more than one test.
    Signed-off-by: Aviel Segev <asegev@redhat.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -22,11 +22,11 @@ var _ = Describe("e2e", func() {
 
 	BeforeEach(func() {
 		svc, err = NewPlannerService(defaultConfigPath)
-		Expect(err).To(BeNil())
+		Expect(err).To(BeNil(), "Failed to create PlannerService")
 		agent, err = NewPlannerAgent(defaultConfigPath, vmName)
-		Expect(err).To(BeNil())
+		Expect(err).To(BeNil(), "Failed to create PlannerAgent")
 		err = agent.Run()
-		Expect(err).To(BeNil())
+		Expect(err).To(BeNil(), "Failed to run PlannerAgent")
 		Eventually(func() string {
 			agentIP, err = agent.GetIp()
 			if err != nil {
@@ -40,7 +40,7 @@ var _ = Describe("e2e", func() {
 		}, "3m", "2s").Should(BeTrue())
 
 		Eventually(func() string {
-			s, err := svc.GetAgent()
+			s, err := svc.GetAgent(fmt.Sprintf("http://%s:3333", agentIP))
 			if err != nil {
 				return ""
 			}
@@ -50,9 +50,19 @@ var _ = Describe("e2e", func() {
 
 			return ""
 		}, "3m", "2s").Should(Equal(fmt.Sprintf("http://%s:3333", agentIP)))
+
+		// Check that planner-agent service is running
+		r := agent.IsServiceRunning(agentIP, "planner-agent")
+		Expect(r).To(BeTrue())
 	})
 
 	AfterEach(func() {
+		s, err := svc.GetAgent(fmt.Sprintf("http://%s:3333", agentIP))
+		if err != nil {
+			s = nil
+		}
+		_ = svc.RemoveAgent(s.Id) // remove the agent created within the 'BeforeEach' block from the DB
+
 		_ = svc.RemoveSources()
 		_ = agent.Remove()
 	})
@@ -61,18 +71,55 @@ var _ = Describe("e2e", func() {
 		agent.DumpLogs(agentIP)
 	})
 
+	Context("Check Vcenter login behavior", func() {
+
+		It("should successfully login with valid credentials", func() {
+
+			res, err := agent.Login(fmt.Sprintf("https://%s:8989/sdk", systemIP), "core", "123456")
+			Expect(err).To(BeNil())
+			Expect(res.StatusCode).To(Equal(http.StatusNoContent))
+
+		})
+
+		It("Two test combined: should return BadRequest due to an empty username"+
+			" and BadRequest due to an empty password", func() {
+
+			res1, err1 := agent.Login(fmt.Sprintf("https://%s:8989/sdk", systemIP), "", "pass")
+			Expect(err1).To(BeNil())
+			Expect(res1.StatusCode).To(Equal(http.StatusBadRequest))
+
+			res2, err2 := agent.Login(fmt.Sprintf("https://%s:8989/sdk", systemIP), "user", "")
+			Expect(err2).To(BeNil())
+			Expect(res2.StatusCode).To(Equal(http.StatusBadRequest))
+
+		})
+
+		It("should return Unauthorized due to invalid credentials", func() {
+
+			res, err := agent.Login(fmt.Sprintf("https://%s:8989/sdk", systemIP), "invalid", "cred")
+			Expect(err).To(BeNil())
+			Expect(res.StatusCode).To(Equal(http.StatusUnauthorized))
+
+		})
+
+		It("should return badRequest due to an invalid URL", func() {
+
+			res, err := agent.Login(fmt.Sprintf("https://%s", systemIP), "user", "pass") // bad link to Vcenter environment
+			Expect(err).To(BeNil())
+			Expect(res.StatusCode).To(Equal(http.StatusBadRequest))
+
+		})
+
+	})
+
 	Context("Flow", func() {
 		It("Up to date", func() {
-			// Check that planner-agent service is running
-			r := agent.IsServiceRunning(agentIP, "planner-agent")
-			Expect(r).To(BeTrue())
-
 			// Put the vCenter credentials and check that source is up to date eventually
-			res, err := agent.Login(fmt.Sprintf("https://%s:8989/sdk", systemIP), "user", "pass")
+			res, err := agent.Login(fmt.Sprintf("https://%s:8989/sdk", systemIP), "core", "123456")
 			Expect(err).To(BeNil())
 			Expect(res.StatusCode).To(Equal(http.StatusNoContent))
 			Eventually(func() bool {
-				apiAgent, err := svc.GetAgent()
+				apiAgent, err := svc.GetAgent(fmt.Sprintf("http://%s:3333", agentIP))
 				if err != nil {
 					return false
 				}
