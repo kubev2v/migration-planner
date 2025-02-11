f1a2f9923deda292c9c0c965df54438240df3581
Cosmin Tupangiu <cosmin@redhat.com>
2025-02-11 10:50:41 +0100

    image: Add implementation for image builder
    Image builder allows a more flexible way to generate ova images.
    It does not change how the image is generated, only provides option to
    parameterize the image generation process.
    
    Signed-off-by: Cosmin Tupangiu <cosmin@redhat.com>

---
+++ internal/image/builder.go
@@ -0,0 +1,322 @@
+package image
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/coreos/butane/config"
+	"github.com/coreos/butane/config/common"
+	"github.com/google/uuid"
+	"github.com/kubev2v/migration-planner/internal/util"
+	"github.com/openshift/assisted-image-service/pkg/isoeditor"
+	"github.com/openshift/assisted-image-service/pkg/overlay"
+)
+
+const (
+	defaultAgentImage            = "quay.io/kubev2v/migration-planner-agent"
+	defaultPlannerService        = "http://127.0.0.1:7443"
+	defaultPersistenceDiskDevice = "/dev/sda"
+	defaultConfigServerUI        = "http://localhost:3000/migrate/wizard"
+	defaultTemplate              = "data/ignition.template"
+	defaultPersistentDiskImage   = "data/persistence-disk.vmdk"
+	defaultOvfFile               = "data/MigrationAssessment.ovf"
+	defaultOvfName               = "MigrationAssessment.ovf"
+	defaultIsoImageName          = "MigrationAssessment.iso"
+	defaultRHCOSImage            = "rhcos-live.x86_64.iso"
+)
+
+type ImageBuilder struct {
+	SourceID                   string
+	SshKey                     string
+	PlannerServiceUI           string
+	PlannerService             string
+	MigrationPlannerAgentImage string
+	InsecureRegistry           string
+	Token                      string
+	PersistentDiskDevice       string
+	PersistentDiskImage        string
+	IsoImageName               string
+	OvfFile                    string
+	OvfName                    string
+	Template                   string
+	RHCOSImage                 string
+}
+
+func NewImageBuilder(sourceID uuid.UUID) *ImageBuilder {
+	imageBuilder := &ImageBuilder{
+		SourceID:                   sourceID.String(),
+		PlannerService:             util.GetEnv("CONFIG_SERVER", defaultPlannerService),
+		PlannerServiceUI:           util.GetEnv("CONFIG_SERVER_UI", defaultConfigServerUI),
+		MigrationPlannerAgentImage: util.GetEnv("MIGRATION_PLANNER_AGENT_IMAGE", defaultAgentImage),
+		PersistentDiskDevice:       util.GetEnv("PERSISTENT_DISK_DEVICE", defaultPersistenceDiskDevice),
+		PersistentDiskImage:        defaultPersistentDiskImage,
+		IsoImageName:               defaultIsoImageName,
+		OvfFile:                    defaultOvfFile,
+		OvfName:                    defaultOvfName,
+		Template:                   defaultTemplate,
+		RHCOSImage:                 defaultRHCOSImage,
+	}
+
+	if insecureRegistry := os.Getenv("INSECURE_REGISTRY"); insecureRegistry != "" {
+		imageBuilder.InsecureRegistry = insecureRegistry
+	}
+
+	return imageBuilder
+}
+
+func (b *ImageBuilder) Generate(ctx context.Context, w io.Writer) (int, error) {
+	ignitionContent, err := b.generateIgnition()
+	if err != nil {
+		return -1, err
+	}
+
+	// Generate ISO data reader with ignition content
+	reader, err := isoeditor.NewRHCOSStreamReader(b.RHCOSImage, &isoeditor.IgnitionContent{Config: []byte(ignitionContent)}, nil, nil)
+	if err != nil {
+		return -1, fmt.Errorf("error reading rhcos iso: %w", err)
+	}
+
+	size, err := b.computeSize(reader)
+	if err != nil {
+		return -1, err
+	}
+
+	tw := tar.NewWriter(w)
+
+	// OVF Must be first file in OVA, to support URL download
+	if err := b.writeOvf(tw); err != nil {
+		return -1, err
+	}
+
+	// Write ISO to TAR
+	if err := b.writeIso(reader, tw); err != nil {
+		return -1, err
+	}
+
+	if err := writePersistenceDisk(tw); err != nil {
+		return -1, err
+	}
+
+	tw.Flush()
+
+	return size, nil
+}
+
+func (b *ImageBuilder) Validate() error {
+	ignitionContent, err := b.generateIgnition()
+	if err != nil {
+		return err
+	}
+
+	// Generate ISO data reader with ignition content
+	if _, err = isoeditor.NewRHCOSStreamReader(b.RHCOSImage, &isoeditor.IgnitionContent{Config: []byte(ignitionContent)}, nil, nil); err != nil {
+		return fmt.Errorf("error reading rhcos iso: %w", err)
+	}
+
+	return nil
+}
+
+func (b *ImageBuilder) generateIgnition() (string, error) {
+	ignData := IgnitionData{
+		SourceID:                   b.SourceID,
+		SshKey:                     b.SshKey,
+		PlannerServiceUI:           b.PlannerServiceUI,
+		PlannerService:             b.PlannerService,
+		MigrationPlannerAgentImage: b.MigrationPlannerAgentImage,
+		InsecureRegistry:           b.InsecureRegistry,
+		Token:                      b.Token,
+		PersistentDiskDevice:       b.PersistentDiskDevice,
+	}
+
+	var buf bytes.Buffer
+	t, err := template.New("ignition.template").ParseFiles(b.Template)
+	if err != nil {
+		return "", fmt.Errorf("error reading the ignition template: %w", err)
+	}
+	if err := t.Execute(&buf, ignData); err != nil {
+		return "", fmt.Errorf("error parsing the ignition template: %w", err)
+	}
+
+	dataOut, _, err := config.TranslateBytes(buf.Bytes(), common.TranslateBytesOptions{})
+	if err != nil {
+		return "", fmt.Errorf("error translating config: %w", err)
+	}
+
+	return string(dataOut), nil
+}
+
+func (b *ImageBuilder) writeIso(reader overlay.OverlayReader, tw *tar.Writer) error {
+	// Create a header for MigrationAssessment.iso
+	length, err := reader.Seek(0, io.SeekEnd)
+	if err != nil {
+		return err
+	}
+	header := &tar.Header{
+		Name:    b.IsoImageName,
+		Size:    length,
+		Mode:    0600,
+		ModTime: time.Now(),
+	}
+
+	// Write the header to the tar archive
+	if err := tw.WriteHeader(header); err != nil {
+		return err
+	}
+
+	// Reset the reader to start
+	_, err = reader.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	// Write ISO data to writer output
+	if _, err = io.Copy(tw, reader); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *ImageBuilder) writeOvf(tw *tar.Writer) error {
+	ovfContent, err := os.ReadFile(b.OvfFile)
+	if err != nil {
+		return err
+	}
+	// Create a header for AgentVM.ovf
+	header := &tar.Header{
+		Name:    b.OvfName,
+		Size:    int64(len(ovfContent)),
+		Mode:    0600,
+		ModTime: time.Now(),
+	}
+
+	// Write the header to the tar archive
+	if err := tw.WriteHeader(header); err != nil {
+		return err
+	}
+	if _, err := tw.Write([]byte(ovfContent)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *ImageBuilder) ovfSize() (int, error) {
+	file, err := os.Open(b.OvfFile)
+	if err != nil {
+		return -1, err
+	}
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return -1, err
+	}
+
+	return calculateTarSize(int(fileInfo.Size())), nil
+}
+
+func (b *ImageBuilder) diskSize() (int, error) {
+	file, err := os.Open(b.PersistentDiskImage)
+	if err != nil {
+		return -1, err
+	}
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return -1, err
+	}
+
+	return calculateTarSize(int(fileInfo.Size())), nil
+}
+
+func (b *ImageBuilder) computeSize(reader overlay.OverlayReader) (int, error) {
+	length, err := reader.Seek(0, io.SeekEnd)
+	if err != nil {
+		return -1, err
+	}
+
+	// Reset the reader to start
+	_, err = reader.Seek(0, io.SeekStart)
+	if err != nil {
+		return -1, err
+	}
+
+	isoSize := calculateTarSize(int(length))
+
+	ovfSize, err := b.ovfSize()
+	if err != nil {
+		return -1, fmt.Errorf("failed to compute ovf size: %s", err)
+	}
+
+	persistentDiskSize, err := b.diskSize()
+	if err != nil {
+		return -1, fmt.Errorf("failed to comute persistent disk size: %s", err)
+	}
+
+	return isoSize + ovfSize + persistentDiskSize, nil
+}
+
+func (b *ImageBuilder) WithSshKey(sshKey string) *ImageBuilder {
+	b.SshKey = sshKey
+	return b
+}
+
+func (b *ImageBuilder) WithPlannerAgentImage(imageUrl string) *ImageBuilder {
+	b.MigrationPlannerAgentImage = imageUrl
+	return b
+}
+
+func (b *ImageBuilder) WithPlannerServiceUI(uiUrl string) *ImageBuilder {
+	b.PlannerServiceUI = uiUrl
+	return b
+}
+
+func (b *ImageBuilder) WithPersistenceDiskDevice(persistenceDevice string) *ImageBuilder {
+	b.PersistentDiskDevice = persistenceDevice
+	return b
+}
+
+func (b *ImageBuilder) WithAgentToken(token string) *ImageBuilder {
+	b.Token = token
+	return b
+}
+
+func (b *ImageBuilder) WithInsecureRegistry(insecureRegistry string) *ImageBuilder {
+	b.InsecureRegistry = insecureRegistry
+	return b
+}
+
+func (b *ImageBuilder) WithTemplate(templatePath string) *ImageBuilder {
+	b.Template = templatePath
+	return b
+}
+
+func (b *ImageBuilder) WithIsoImageName(name string) *ImageBuilder {
+	b.IsoImageName = name
+	return b
+}
+
+func (b *ImageBuilder) WithPersistentDiskImage(imagePath string) *ImageBuilder {
+	b.PersistentDiskImage = imagePath
+	return b
+}
+
+func (b *ImageBuilder) WithOvfFile(ovfFile string) *ImageBuilder {
+	b.OvfFile = ovfFile
+	return b
+}
+
+func (b *ImageBuilder) WithOvfName(ovfName string) *ImageBuilder {
+	b.OvfName = ovfName
+	return b
+}
+
+func (b *ImageBuilder) WithRHCOSImage(image string) *ImageBuilder {
+	b.RHCOSImage = image
+	return b
+}
--- internal/service/image.go
+++ internal/service/image.go
@@ -31,29 +31,28 @@ func (h *ServiceHandler) GetImage(ctx context.Context, request server.GetImageRe
 		return server.GetImage500JSONResponse{}, nil
 	}
 
-	ova := &image.Ova{SourceID: source.ID, SshKey: source.SshPublicKey, Writer: writer}
-	// get token if any
-	if user, found := auth.UserFromContext(ctx); found {
-		ova.Jwt = user.Token
+	imageBuilder := image.NewImageBuilder(source.ID)
+
+	if source.SshPublicKey != nil {
+		imageBuilder = imageBuilder.WithSshKey(*source.SshPublicKey)
 	}
 
-	// Calculate the size of the OVA, so the download show estimated time:
-	size, err := ova.OvaSize()
+	if user, found := auth.UserFromContext(ctx); found {
+		if user.Token != nil {
+			imageBuilder = imageBuilder.WithAgentToken(user.Token.Raw)
+		}
+	}
+
+	size, err := imageBuilder.Generate(ctx, writer)
 	if err != nil {
-		zap.S().Named("image_service").Errorf("error calculating OvaSize at GetImage: %s", err)
-		return server.GetImage500JSONResponse{Message: "error creating the HTTP stream"}, nil
+		metrics.IncreaseOvaDownloadsTotalMetric("failed")
+		zap.S().Named("image_service").Errorf("error generating ova at GetImage: %s", err)
+		return server.GetImage500JSONResponse{Message: fmt.Sprintf("error generating image %s", err)}, nil
 	}
 
 	// Set proper headers of the OVA file:
 	writer.Header().Set("Content-Type", "application/ovf")
 	writer.Header().Set("Content-Length", strconv.Itoa(size))
-
-	// Generate the OVA image
-	if err := ova.Generate(); err != nil {
-		metrics.IncreaseOvaDownloadsTotalMetric("failed")
-		zap.S().Named("image_service").Errorf("error generating ova at GetImage: %s", err)
-		return server.GetImage500JSONResponse{Message: fmt.Sprintf("error generating image %s", err)}, nil
-	}
 
 	metrics.IncreaseOvaDownloadsTotalMetric("successful")
 
@@ -61,11 +60,6 @@ func (h *ServiceHandler) GetImage(ctx context.Context, request server.GetImageRe
 }
 
 func (h *ServiceHandler) HeadImage(ctx context.Context, request server.HeadImageRequestObject) (server.HeadImageResponseObject, error) {
-	writer, ok := ctx.Value(image.ResponseWriterKey).(http.ResponseWriter)
-	if !ok {
-		return server.HeadImage400Response{}, nil
-	}
-
 	source, err := h.store.Source().Get(ctx, request.Id)
 	if err != nil {
 		if errors.Is(err, store.ErrRecordNotFound) {
@@ -74,10 +68,23 @@ func (h *ServiceHandler) HeadImage(ctx context.Context, request server.HeadImage
 		return server.HeadImage500Response{}, nil
 	}
 
-	ova := &image.Ova{SourceID: request.Id, SshKey: source.SshPublicKey, Writer: writer}
-	if err := ova.Validate(); err != nil {
+	imageBuilder := image.NewImageBuilder(source.ID)
+
+	if source.SshPublicKey != nil {
+		imageBuilder = imageBuilder.WithSshKey(*source.SshPublicKey)
+	}
+
+	// get token if any
+	if user, found := auth.UserFromContext(ctx); found {
+		if user.Token != nil {
+			imageBuilder = imageBuilder.WithAgentToken(user.Token.Raw)
+		}
+	}
+
+	if err := imageBuilder.Validate(); err != nil {
 		zap.S().Named("image_service").Errorf("error validating at HeadImage: %s", err)
 		return server.HeadImage500Response{}, nil
 	}
+
 	return server.HeadImage200Response{}, nil
 }
