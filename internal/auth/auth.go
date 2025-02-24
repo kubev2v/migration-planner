93a72327bb94e466c81668359672524905ad41d9
Nir Argaman <nargaman@redhat.com>
2025-02-24 13:30:00 +1100

    Change configuration to use envconfig
    Signed-off-by: Nir Argaman <nargaman@redhat.com>

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,27 +1,33 @@
 package auth
 
 import (
-	"net/http"
-
-	"github.com/kubev2v/migration-planner/internal/config"
 	"go.uber.org/zap"
+	"net/http"
 )
+
+type AuthType string
+
+const (
+	AuthTypeEmpty AuthType = ""
+	AuthTypeRHSSO AuthType = "rhsso"
+	AuthTypeNone  AuthType = "none"
+)
+
+type Config struct {
+	AuthType   AuthType `envconfig:"MIGRATION_PLANNER_AUTH" default:""`
+	JwtCertUrl string   `envconfig:"MIGRATION_PLANNER_JWK_URL" default:""`
+}
 
 type Authenticator interface {
 	Authenticator(next http.Handler) http.Handler
 }
 
-const (
-	RHSSOAuthentication string = "rhsso"
-	NoneAuthentication  string = "none"
-)
+func NewAuthenticator(cfg Config) (Authenticator, error) {
+	zap.S().Named("auth").Infof("authentication: '%s'", cfg.AuthType)
 
-func NewAuthenticator(authConfig config.Auth) (Authenticator, error) {
-	zap.S().Named("auth").Infof("authentication: '%s'", authConfig.AuthenticationType)
-
-	switch authConfig.AuthenticationType {
-	case RHSSOAuthentication:
-		return NewRHSSOAuthenticator(authConfig.JwkCertURL)
+	switch cfg.AuthType {
+	case AuthTypeRHSSO:
+		return NewRHSSOAuthenticator(cfg.JwtCertUrl)
 	default:
 		return NewNoneAuthenticator()
 	}
