40d5f39b53b5550bb1d37b5a616d0e833d7b92c8
Ronen <ravraham@redhat.com>
2025-03-05 17:10:40 +0200

    A workaround to fix podman-compose absolute path. Bug on podman-compose 1.3.0: https://github.com/containers/podman-compose/issues/1109
    Signed-off-by: Ronen <ravraham@redhat.com>

--- deploy/deploy.mk
+++ deploy/deploy.mk
@@ -1,12 +1,14 @@
+COMPOSE_FILE := $(realpath deploy/podman/compose.yaml)
+
 deploy-db:
 	podman rm -f planner-db || true
 	podman volume rm podman_planner-db || true
-	podman-compose -f deploy/podman/compose.yaml up -d planner-db
+	podman-compose -f $(COMPOSE_FILE) up -d planner-db
 	test/scripts/wait_for_postgres.sh podman
 	podman exec -it planner-db psql -c 'ALTER ROLE admin WITH SUPERUSER'
 	podman exec -it planner-db createdb admin || true
 
 kill-db:
-	podman-compose -f deploy/podman/compose.yaml down planner-db
+	podman-compose -f $(COMPOSE_FILE) down planner-db
 
 .PHONY: deploy-db kill-db
