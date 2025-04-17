96bb6461e942da90d3a0acc908e40a31f68773d8
Ondra Machacek <omachace@redhat.com>
2025-04-17 12:44:12 +0200

    Fix sending the headers for image download
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- internal/service/image.go
+++ internal/service/image.go
@@ -65,16 +65,23 @@ func (h *ServiceHandler) GetImage(ctx context.Context, request server.GetImageRe
 		return server.GetImage500JSONResponse{}, nil
 	}
 
-	size, err := imageBuilder.Generate(ctx, writer)
+	// Get image size
+	size, err := imageBuilder.Size()
+	if err != nil {
+		return server.GetImage500JSONResponse{Message: fmt.Sprintf("failed to read image size %s", err)}, nil
+	}
+
+	// Set proper headers of the OVA file:
+	writer.Header().Set("Content-Type", "application/ovf")
+	writer.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fmt.Sprintf("%s.ova", source.Name)))
+	writer.Header().Set("Content-Length", strconv.Itoa(size))
+
+	err = imageBuilder.Generate(ctx, writer)
 	if err != nil {
 		metrics.IncreaseOvaDownloadsTotalMetric("failed")
 		zap.S().Named("image_service").Errorf("failed to generate ova at GetImage: %s", err)
 		return server.GetImage500JSONResponse{Message: fmt.Sprintf("failed to generate image %s", err)}, nil
 	}
-
-	// Set proper headers of the OVA file:
-	writer.Header().Set("Content-Type", "application/ovf")
-	writer.Header().Set("Content-Length", strconv.Itoa(size))
 
 	metrics.IncreaseOvaDownloadsTotalMetric("successful")
 
