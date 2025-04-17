97af407c93103af643d1958274a01da46d9c9e8e
Cosmin Tupangiu <cosmin@redhat.com>
2025-04-17 09:58:00 +0200

    remove event producer
    Since we don't push events to kafka anymore, the event producer has been
    dead code.
    This commit removes the event producer and API `/api/v1/events`
    endpoint.
    
    Signed-off-by: Cosmin Tupangiu <cosmin@redhat.com>
    Signed-off-by: Ronen Avraham <ravraham@redhat.com>

--- api/v1alpha1/types.gen.go
+++ api/v1alpha1/types.gen.go
@@ -54,18 +54,6 @@ type Error struct {
 	Message string `json:"message"`
 }
 
-// Event defines model for Event.
-type Event struct {
-	CreatedAt time.Time   `json:"createdAt"`
-	Data      []EventData `json:"data"`
-}
-
-// EventData defines model for EventData.
-type EventData struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
 // Infra defines model for Infra.
 type Infra struct {
 	Datastores []struct {
@@ -189,9 +177,6 @@ type ListSourcesParams struct {
 	// IncludeDefault control whatever the default report should be added to the result
 	IncludeDefault *bool `form:"include_default,omitempty" json:"include_default,omitempty"`
 }
-
-// PushEventsJSONRequestBody defines body for PushEvents for application/json ContentType.
-type PushEventsJSONRequestBody = Event
 
 // CreateSourceJSONRequestBody defines body for CreateSource for application/json ContentType.
 type CreateSourceJSONRequestBody = SourceCreate
--- internal/events/options.go
+++
@@ -1,9 +0,0 @@
-package events
-
-type ProducerOptions func(e *EventProducer)
-
-func WithOutputTopic(topic string) ProducerOptions {
-	return func(e *EventProducer) {
-		e.topic = topic
-	}
-}
