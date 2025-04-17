15322be344e5e735e9e3e8b6be4801bf8c95b1a7
Aviel Segev <asegev@redhat.com>
2025-04-17 23:10:06 +0300

    [E2E] Add Multiple-users test case
    Signed-off-by: Aviel Segev <asegev@redhat.com>

--- test/e2e/e2e_agent/agent_api.go
+++ test/e2e/e2e_agent/agent_api.go
@@ -12,31 +12,32 @@ import (
 	"net/http"
 )
 
+// AgentApi provides a client to interact with the Planner Agent API
 type AgentApi struct {
 	baseURL    string
 	httpClient *http.Client
 }
 
+// DefaultAgentApi creates an AgentApi client with a default HTTP client that skips TLS verification
 func DefaultAgentApi(agentApiBaseUrl string) *AgentApi {
-	return &AgentApi{
-		baseURL: agentApiBaseUrl,
-		httpClient: &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
+	return NewAgentApi(agentApiBaseUrl, &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
 			},
 		},
-	}
+	})
 }
 
-func CustomAgentApi(agentApiBaseUrl string, customHttpClient *http.Client) *AgentApi {
+// NewAgentApi creates an AgentApi client with a custom HTTP client, useful for test customization
+func NewAgentApi(agentApiBaseUrl string, customHttpClient *http.Client) *AgentApi {
 	return &AgentApi{
 		baseURL:    agentApiBaseUrl,
 		httpClient: customHttpClient,
 	}
 }
 
+// request is a helper to send an HTTP request to the agent and unmarshal the response into given struct
 func (api *AgentApi) request(method string, path string, body []byte, result any) (*http.Response, error) {
 	var req *http.Request
 	var err error
@@ -79,6 +80,7 @@ func (api *AgentApi) request(method string, path string, body []byte, result any
 	return res, nil
 }
 
+// Version retrieves the agent's version string
 func (api *AgentApi) Version() (string, error) {
 	var result struct {
 		Version string `json:"version"`
@@ -91,6 +93,7 @@ func (api *AgentApi) Version() (string, error) {
 	return result.Version, nil
 }
 
+// Login put the vCenter credentials
 func (api *AgentApi) Login(url string, user string, pass string) (*http.Response, error) {
 	zap.S().Infof("Attempting vCenter login with URL: %s, User: %s", url, user)
 
@@ -113,6 +116,7 @@ func (api *AgentApi) Login(url string, user string, pass string) (*http.Response
 	return res, nil
 }
 
+// Status retrieves the current status of the agent
 func (api *AgentApi) Status() (*coreAgent.StatusReply, error) {
 	result := &coreAgent.StatusReply{}
 	res, err := api.request(http.MethodGet, "status", nil, result)
@@ -124,6 +128,7 @@ func (api *AgentApi) Status() (*coreAgent.StatusReply, error) {
 	return result, nil
 }
 
+// Inventory retrieves the inventory data collected by the agent
 func (api *AgentApi) Inventory() (*v1alpha1.Inventory, error) {
 	var result struct {
 		Inventory v1alpha1.Inventory `json:"inventory"`
