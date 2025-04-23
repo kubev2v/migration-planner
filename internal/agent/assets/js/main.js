0ef43c2ab6b6b1580ea7c9d0c96fe41f8f70eacf
Cosmin Tupangiu <cosmin@redhat.com>
2025-04-23 07:24:36 +0200

    wip
    Signed-off-by: Cosmin Tupangiu <cosmin@redhat.com>
    Signed-off-by: Ronen Avraham <ravraham@redhat.com>

---
+++ internal/agent/assets/js/main.js
@@ -0,0 +1,12 @@
+import createStore from './store/index.js';
+
+const loginFormContainer = document.querySelector("#loginFormContainer");
+const store = createStore();
+
+// Set things up
+window.addEventListener(
+    "load",
+    () => {
+    },
+    false,
+);
