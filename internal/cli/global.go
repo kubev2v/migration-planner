1675d23a49163f3460fbee5857a4ce10921b6f67
Aviel Segev <128503711+AvielSegev@users.noreply.github.com>
2025-07-08 10:51:39 +0300

    Fix the auth header in the CLI and add the option to pass proxy address (#367)
    Signed-off-by: Aviel Segev <asegev@redhat.com>

--- internal/cli/global.go
+++ internal/cli/global.go
@@ -2,10 +2,12 @@ package cli
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/kubev2v/migration-planner/internal/api/client"
-	"github.com/kubev2v/migration-planner/internal/auth"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
@@ -13,6 +15,7 @@ import (
 type GlobalOptions struct {
 	ServerUrl string
 	Token     string
+	ProxyUrl  string
 }
 
 func DefaultGlobalOptions() GlobalOptions {
@@ -24,6 +27,7 @@ func DefaultGlobalOptions() GlobalOptions {
 func (o *GlobalOptions) Bind(fs *pflag.FlagSet) {
 	fs.StringVarP(&o.ServerUrl, "server-url", "u", o.ServerUrl, "Address of the server")
 	fs.StringVarP(&o.Token, "token", "", o.Token, "Token used to authenticate the user")
+	fs.StringVar(&o.ProxyUrl, "proxy", "", "Address of the proxy")
 }
 
 func (o *GlobalOptions) Complete(cmd *cobra.Command, args []string) error {
@@ -35,15 +39,42 @@ func (o *GlobalOptions) Validate(args []string) error {
 }
 
 func (o *GlobalOptions) Client() (*client.ClientWithResponses, error) {
+	httpClient := &http.Client{}
+
+	if o.ProxyUrl != "" {
+		if err := o.WithProxy(httpClient); err != nil {
+			return nil, err
+		}
+	}
+
 	return client.NewClientWithResponses(
 		o.ServerUrl,
-		client.WithHTTPClient(&http.Client{}),
+		client.WithHTTPClient(httpClient),
 		client.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
 			if o.Token == "" {
 				return nil
 			}
-			req.Header.Add(auth.AgentTokenHeader, o.Token)
+			req.Header.Set("X-Authorization", fmt.Sprintf("Bearer %s", o.Token))
 			return nil
 		}),
 	)
+}
+
+func (o *GlobalOptions) WithProxy(httpClient *http.Client) error {
+	if !strings.HasPrefix(o.ProxyUrl, "http://") && !strings.HasPrefix(o.ProxyUrl, "https://") {
+		o.ProxyUrl = "http://" + o.ProxyUrl
+	}
+
+	proxyUrl, err := url.Parse(o.ProxyUrl)
+	if err != nil {
+		return err
+	}
+
+	transport := &http.Transport{
+		Proxy: http.ProxyURL(proxyUrl),
+	}
+
+	httpClient.Transport = transport
+
+	return nil
 }
