f126112d31c9da7059397fe62a1dbcdd5e659bce
Nir Argaman <nargaman@redhat.com>
2025-07-31 10:34:44 +1000

    Makefile: Organize test targets and add Emojis to Makefile Targets
    Signed-off-by: Nir Argaman <nargaman@redhat.com>

--- deploy/deploy.mk
+++ deploy/deploy.mk
@@ -1,14 +1,18 @@
 COMPOSE_FILE := $(realpath deploy/podman/compose.yaml)
 
 deploy-db:
+	@echo "🚀 Deploy DB on podman..."
 	podman rm -f planner-db || true
 	podman volume rm podman_planner-db || true
 	podman-compose -f $(COMPOSE_FILE) up -d planner-db
 	test/scripts/wait_for_postgres.sh podman
 	podman exec -it planner-db psql -c 'ALTER ROLE admin WITH SUPERUSER'
 	podman exec -it planner-db createdb admin || true
+	@echo "✅ DB was deployed successfully on podman."
 
 kill-db:
+	@echo "🗑️ Remove DB instance from podman..."
 	podman-compose -f $(COMPOSE_FILE) down planner-db
+	@echo "✅ DB instance was removed successfully from podman."
 
 .PHONY: deploy-db kill-db
