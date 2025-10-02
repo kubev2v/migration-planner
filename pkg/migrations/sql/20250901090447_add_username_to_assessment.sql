81825f02525930d51102f9753c7440389b9df5f6
Ondra Machacek <omachace@redhat.com>
2025-10-02 15:24:17 +0200

    ECOPROJECT-3343 | fix: Support upgrade from prod to current main
    This PR modifies the goose migration scripts, so we can support the upgrade
    from branch release_0.1.4-1. Which contained 0250926140958 but didn't contain
    previous migration script so it wasn't possible to migrate to current DB.
    
    This PR create a new migration scripts, which union the previuos scripts,
    so upgrade and also new DB creation works.
    
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- pkg/migrations/sql/20250901090447_add_username_to_assessment.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE assessments ADD COLUMN username VARCHAR(255);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE assessments DROP COLUMN username;
--- +goose StatementEnd
