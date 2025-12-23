e12120720e22a0a537aeb107b0db2875bcb3b105
Cosmin Tupangiu <cosmin@redhat.com>
2025-12-23 11:30:26 +0100

    NO-JIRA | fix: Fix constraint of the assessment uniqueness
    The initial constraint (username,name) fails because some users created
    assessments having the same name, same username but different org_id.
    This is part of the testing different features.
    
    Signed-off-by: Cosmin Tupangiu <cosmin@redhat.com>

--- pkg/migrations/sql/20251219125345_drop_assessment_org_id_constraint.sql
+++ pkg/migrations/sql/20251219125345_drop_assessment_org_id_constraint.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE assessments DROP CONSTRAINT org_id_name;
-ALTER TABLE assessments ADD CONSTRAINT user_name UNIQUE (username, name);
+ALTER TABLE assessments ADD CONSTRAINT org_id_user_name UNIQUE (org_id, username, name);
 -- +goose StatementEnd
 
 -- +goose Down
