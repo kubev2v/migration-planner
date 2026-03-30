d476f5c91a42334614d419744b68bdb587076209
Cosmin Tupangiu <cosmin@redhat.com>
2026-03-30 08:33:52 +0200

    NO-JIRA | fix: remove the example assessment
    Signed-off-by: Cosmin Tupangiu <cosmin@redhat.com>

--- pkg/migrations/sql/20260325120001_backfill_owner_relations.sql
+++ pkg/migrations/sql/20260325120001_backfill_owner_relations.sql
@@ -1,5 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
+DELETE FROM assessments WHERE username is null and org_id = 'example';
 INSERT INTO relations (resource, resource_id, relation, subject_namespace, subject_id)
 SELECT 'assessment', a.id::text, 'owner', 'user', a.username
 FROM assessments a
